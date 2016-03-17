7acb779d87734cfaeeba37c7e74be59b8d066083
kandis brighton <kandisbrighton@gmail.com>
2016-03-17 12:25:51 -0700

    Kent Food Bank Website by Scrum Team Kaneses

---
+++ kfb_apply_form.php
@@ -0,0 +1,63 @@
+<?php include "kfb_head.php" ?>
+
+<body class="no-trans kfb_background">
+
+	
+<?php include "kfb_menubar.php" ?>
+
+ <div class="page-wrapper" >
+
+		<!-- main-container start -->
+			<!-- ================ -->
+			<section class="main-container kfb_background">
+
+				<div class="container">
+					<div class="row">
+						
+<!-- Think carefully before modifying code above this line  -->
+
+					<!-- KFB page content starts here  -->
+					
+						<div class="main col-md-9">
+							
+						<div id = pageblock1 class = "pageblock">
+							<h2>Volunteer Application Form</h2>
+							
+						</div> 
+						
+						<div id = pageblock2 class = "pageblock">
+						<?php include "kfb_apply_form_include.php" ?>
+						</div>
+						
+	
+							
+				</div>
+					<!-- KFB page content ends here  -->
+					
+<!-- Think carefully before modifying code below this line  -->
+
+						<!-- sidebar start -->
+						<!-- ================ -->
+						
+							<?php #include "kfb_sidebar_bozhang.php" ?>
+						
+						<!-- sidebar end -->
+
+					</div>
+				
+				</div>
+			
+			</section>
+			<!-- main-container end -->
+			
+			
+			
+		</div>
+		<!-- page-wrapper end -->
+		</div>
+ 			<!-- sidebar start -->
+
+
+<?php include "kfb_footer.html" ?>
+</body>
+</html>
---
+++ kfb_apply_form_framework.php
@@ -0,0 +1,52 @@
+<?php include "kfb_head.php" ?>
+
+<body class="no-trans   ">
+    
+<?php include "kfb_menubar.php" ?>
+ <div class="page-wrapper">
+
+			<!-- main-container start -->
+			<!-- ================ -->
+			<section class="main-container">
+
+				<div class="container">
+					<div class="row">
+						
+<!-------- Think carefully before modifying code above this line  ----->
+
+					<!-- Add KFB page content starts here  -->
+						<div class="main col-md-9">
+							
+						
+						
+						<div id = pageblock2 class = "pageblock">
+
+			
+						<?php include "kfb_apply_form_include.php" ?>
+				
+						</div>
+							
+							
+					<!-- Add KFB page content ends here  -->
+					
+<!-------- Think carefully before modifying code below this line  ----->
+
+						<!-- sidebar start -->
+						<!-- ================ -->
+						<?php include "kfb_sidebar.php" ?>
+						<!-- sidebar end -->
+
+					</div>
+				</div>
+			</section>
+			<!-- main-container end -->
+			
+
+
+			
+		</div>
+		<!-- page-wrapper end -->
+ </div>
+<?php include "kfb_footer.html" ?>
+</body>
+<html>
